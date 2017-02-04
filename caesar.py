cb8951a2aa5e84c56fd6e96b75170d07d52ee305
BoysKungen <alex.henryz@itggot.se>
2017-02-04 11:09:20 +0100

    Added File

---
+++ caesar.py
@@ -0,0 +1,22 @@
+import win32api
+
+def caesar():
+	key = input("Enter Key: ")
+	array = input("Enter message: ")
+	finish = []
+	space = 32
+
+	for i in list(array):
+		if ord('z') < ord(i) + int(key):
+			left = (ord(i) + int(key))- ord('z')
+			rest = ord('a') + int(left - 1)
+			finish.append(chr(rest))
+		elif ord(i) == space:
+			finish.append(chr(space))
+		else:
+			b = ord(i) + int(key)
+			c = chr(b)
+			finish.append(c)
+	print(finish)
+
+caesar()
